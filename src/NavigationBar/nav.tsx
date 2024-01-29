15eae0c16172c750d8187177f5d113e5bea93a28
Brandon <brandon@mauldin314.com>
2024-01-28 18:23:46 -0800

    Added character limit setter

--- src/NavigationBar/nav.tsx
+++ src/NavigationBar/nav.tsx
@@ -1,18 +1,31 @@
 import Navbar from "react-bootstrap/Navbar";
-import DarkModeButton from "./DarkMode";
 import "./NavigationBar.css";
+// import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
-function NavigationBar() {
+interface navInterface {
+  setCharacterLimit: React.Dispatch<React.SetStateAction<number>>;
+}
+
+function NavigationBar(props: navInterface) {
   return (
     <div className="nav_filler">
-      <ul className="nav_bar">
-        <li>
-          <Navbar sticky="top" id="header">
-            <h1>Package Writer</h1>
-          </Navbar>
-        </li>
-        {/* <li><DarkModeButton /></li> */}
-      </ul>
+      <Navbar sticky="top" id="header">
+        <h1>Package Writer</h1>
+      </Navbar>
+      <div>
+        Max Characters:
+        <select
+          defaultValue={350}
+          onChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
+            props.setCharacterLimit(parseInt(e.target.value))
+          }
+        >
+          <option value={250}>250</option>
+          <option value={350}>350</option>
+          <option value={700}>700</option>
+          <option value={-1}>No Limit</option>
+        </select>
+      </div>
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import NavigationBar from "./NavigationBar/nav";
 import App from "./App";
@@ -6,7 +6,6 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <NavigationBar />
     <App />
   </React.StrictMode>
 );
